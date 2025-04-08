2f323b0e94c910fb77b451c19f116bb21c146718
aleosorio22 <alejandroosorio022@gmail.com>
2025-04-08 16:52:21 -0600

    fix: Modulo de pedidos, error con las fechas, panel admin

--- frontend/src/components/client/OrderCard.jsx
+++ frontend/src/components/client/OrderCard.jsx
@@ -1,7 +1,9 @@
 import { FiCalendar, FiUser, FiInfo, FiClock } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import { formatDate } from '../../utils/dateUtils';
 
-export default function OrderCard({ order, formatDate, getStatusColor }) {
+// Modificar props para eliminar formatDate
+export default function OrderCard({ order, getStatusColor }) {
   const navigate = useNavigate();
 
   if (!order) return null;
